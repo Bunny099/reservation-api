a5b8a8937ad5b2eba68dc73181eb5abf11df3081
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-17 21:05:24 +0530

    prisma migrations

---
+++ prisma/migrations/20260116111146_reservation_database_first_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateEnum
+CREATE TYPE "STATUS" AS ENUM ('ACTIVE', 'EXPIRED', 'CANCELLED');
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "capacity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reservation" (
+    "id" TEXT NOT NULL,
+    "roomId" TEXT NOT NULL,
+    "status" "STATUS" NOT NULL DEFAULT 'ACTIVE',
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
